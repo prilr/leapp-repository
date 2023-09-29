6ce19752ca050134d78faa6ee69e4948170b4a54
Roman Prilipskii <rprilipskii@cloudlinux.com>
2023-09-29 13:18:52 +0400

    Update DNF vars in addition to YUM vars

--- repos/system_upgrade/el7toel8/actors/updateyumvars/libraries/updateyumvars.py
+++ repos/system_upgrade/el7toel8/actors/updateyumvars/libraries/updateyumvars.py
@@ -2,22 +2,23 @@ import os
 
 from leapp.libraries.stdlib import api
 
-VAR_FOLDER = "/etc/yum/vars"
+VAR_FOLDERS = ["/etc/yum/vars", "/etc/dnf/vars/"]
 
 
 def vars_update():
     """ Iterate through and modify the variables. """
-    if not os.path.isdir(VAR_FOLDER):
-        api.current_logger().debug(
-            "The {} directory doesn't exist. Nothing to do.".format(VAR_FOLDER)
-        )
-        return
+    for var_folder in VAR_FOLDERS:
+        if not os.path.isdir(var_folder):
+            api.current_logger().debug(
+                "The {} directory doesn't exist. Skipping to next.".format(var_folder)
+            )
+            continue
 
-    for varfile_name in os.listdir(VAR_FOLDER):
-        # cp_centos_major_version contains the current OS' major version.
-        if varfile_name == 'cp_centos_major_version':
-            varfile_path = os.path.join(VAR_FOLDER, varfile_name)
+        for varfile_name in os.listdir(var_folder):
+            # cp_centos_major_version contains the current OS' major version.
+            if varfile_name == 'cp_centos_major_version':
+                varfile_path = os.path.join(var_folder, varfile_name)
 
-            with open(varfile_path, 'w') as varfile:
-                # Overwrite the value from outdated "7".
-                varfile.write('8')
+                with open(varfile_path, 'w') as varfile:
+                    # Overwrite the value from outdated "7".
+                    varfile.write('8')
