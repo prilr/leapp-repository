425999080f659baa40ffe89c440e6b081364999f
Roman Prilipskii <rprilipskii@cloudlinux.com>
2024-02-09 09:46:59 +0400

    Add has_package directly to ClearPackageConflicts, documentation

--- repos/system_upgrade/cloudlinux/actors/clearpackageconflicts/actor.py
+++ repos/system_upgrade/cloudlinux/actors/clearpackageconflicts/actor.py
@@ -3,7 +3,6 @@ import errno
 import shutil
 
 from leapp.actors import Actor
-from leapp.libraries.common.rpms import has_package
 from leapp.models import InstalledRPM
 from leapp.tags import DownloadPhaseTag, IPUWorkflowTag
 from leapp.libraries.common.cllaunch import run_on_cloudlinux
@@ -19,14 +18,40 @@ class ClearPackageConflicts(Actor):
     produces = ()
     tags = (DownloadPhaseTag.Before, IPUWorkflowTag)
 
+    def __init__(self):
+        self._rpm_lookup = None
+
+    @property
+    def rpm_lookup(self):
+        """
+        Get and store the list of installed RPMs for quick lookup.
+        """
+        if not self._rpm_lookup:
+            self._rpm_lookup = {rpm for rpm in self.consume(InstalledRPM)}
+        return self._rpm_lookup
+
+    def has_package(self, name):
+        """
+        Check whether the package is installed.
+        Looks only for the package name, nothing else.
+        """
+        return name in self.rpm_lookup
+
     def problem_packages_installed(self, problem_packages):
+        """
+        Check whether any of the problem packages are present in the system.
+        """
         for pkg in problem_packages:
-            if has_package(InstalledRPM, pkg):
+            if self.has_package(pkg):
                 self.log.debug("Conflicting package {} detected".format(pkg))
                 return True
         return False
 
     def clear_problem_files(self, problem_files, problem_dirs):
+        """
+        Go over the list of problem files and directories and remove them if they exist.
+        They'll be replaced by the new packages.
+        """
         for p_dir in problem_dirs:
             try:
                 if os.path.isdir(p_dir):
@@ -46,6 +71,9 @@ class ClearPackageConflicts(Actor):
                     raise
 
     def alt_python37_handle(self):
+        """
+        These alt-python37 packages are conflicting with their own builds for EL8.
+        """
         problem_packages = [
             "alt-python37-six",
             "alt-python37-pytz",
@@ -60,6 +88,10 @@ class ClearPackageConflicts(Actor):
             self.clear_problem_files(problem_files, problem_dirs)
 
     def openssl_handle(self):
+        """
+        openssl11-libs package from the EPEL repo is conflicting with the incoming openssl-libs package for EL8.
+        https://access.redhat.com/solutions/6986997
+        """
         problem_packages = [
             "openssl11-libs"
         ]
