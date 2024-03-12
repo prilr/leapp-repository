e92cc475f5b4ef1c9f0a45db9200c61bf8811b95
Roman Prilipskii <rprilipskii@cloudlinux.com>
2024-03-12 04:07:48 +0400

    Replace outdated model usage in CLN-related tasks

--- repos/system_upgrade/cloudlinux/actors/checkcllicense/actor.py
+++ repos/system_upgrade/cloudlinux/actors/checkcllicense/actor.py
@@ -6,8 +6,7 @@ from leapp.libraries.stdlib import CalledProcessError, run, api
 from leapp.libraries.common.cllaunch import run_on_cloudlinux
 
 from leapp.models import (
-    RequiredTargetUserspacePackages,
-    RequiredUpgradeInitramPackages,
+    TargetUserSpacePreupgradeTasks,
     TargetUserSpaceUpgradeTasks,
     CopyFile
 )
@@ -29,8 +28,7 @@ def rhn_to_target_userspace():
             if os.path.isfile(src_path):
                 files_to_copy.append(CopyFile(src=src_path))
 
-    api.produce(RequiredTargetUserspacePackages(packages=REQUIRED_PKGS))
-    api.produce(RequiredUpgradeInitramPackages(packages=REQUIRED_PKGS))
+    api.produce(TargetUserSpacePreupgradeTasks(install_rpms=REQUIRED_PKGS, copy_files=files_to_copy))
     api.produce(TargetUserSpaceUpgradeTasks(install_rpms=REQUIRED_PKGS, copy_files=files_to_copy))
 
 
@@ -41,7 +39,7 @@ class CheckClLicense(Actor):
 
     name = 'check_cl_license'
     consumes = ()
-    produces = (Report, RequiredTargetUserspacePackages, RequiredUpgradeInitramPackages, TargetUserSpaceUpgradeTasks)
+    produces = (Report, TargetUserSpacePreupgradeTasks, TargetUserSpaceUpgradeTasks)
     tags = (ChecksPhaseTag, IPUWorkflowTag)
 
     system_id_path = '/etc/sysconfig/rhn/systemid'
