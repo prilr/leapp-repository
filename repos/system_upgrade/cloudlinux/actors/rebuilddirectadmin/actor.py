c2aa65bdd9a5918c9ca6a7305a6a1bacd7b9a5ec
Oleksandr Shyshatskyi <oshyshatskyi@cloudlinux.com>
2024-06-05 12:51:41 +0300

    Change detection of DirectAdmin to detectcontrolpanel lib

--- repos/system_upgrade/cloudlinux/actors/rebuilddirectadmin/actor.py
+++ repos/system_upgrade/cloudlinux/actors/rebuilddirectadmin/actor.py
@@ -5,6 +5,9 @@ from leapp.libraries.stdlib import run, CalledProcessError
 from leapp.reporting import Report, create_report
 from leapp.tags import FirstBootPhaseTag, IPUWorkflowTag
 from leapp.libraries.common.cllaunch import run_on_cloudlinux
+from leapp.models import InstalledControlPanel
+
+from leapp.libraries.common.detectcontrolpanel import DIRECTADMIN_NAME
 
 
 class UpdateDirectAdmin(Actor):
@@ -13,21 +16,27 @@ class UpdateDirectAdmin(Actor):
     """
 
     name = 'update_directadmin'
-    consumes = ()
+    consumes = (InstalledControlPanel,)
     produces = (Report,)
     tags = (FirstBootPhaseTag, IPUWorkflowTag)
 
     @run_on_cloudlinux
     def process(self):
-        if os.path.exists('/bin/da'):
-            try:
-                run(['/bin/da', 'build', 'all'], checked=True)
-                self.log.info('DirectAdmin update was successful')
-            except CalledProcessError as e:
-                self.log.error(
-                    'Command "da build all" finished with exit code {}, '
-                    'the system might be unstable.\n'
-                    'Check /usr/local/directadmin/custombuild/custombuild.log, '
-                    'rerun "da build all" after fixing the issues. '
-                    'Contact DirectAdmin support for help.'.format(e.exit_code)
-                )
+        panel = next(self.consume(InstalledControlPanel), None)
+        if panel is None:
+            raise StopActorExecutionError(message=("Missing information about the installed web panel."))
+
+        if panel.name != DIRECTADMIN_NAME:
+            self.log.debug('DirectAdmin not detected, skip rebuilding')
+
+        try:
+            run(['/bin/da', 'build', 'all'], checked=True)
+            self.log.info('DirectAdmin update was successful')
+        except CalledProcessError as e:
+            self.log.error(
+                'Command "da build all" finished with exit code {}, '
+                'the system might be unstable.\n'
+                'Check /usr/local/directadmin/custombuild/custombuild.log, '
+                'rerun "da build all" after fixing the issues. '
+                'Contact DirectAdmin support for help.'.format(e.exit_code)
+            )
